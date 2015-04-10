73238b5c99999b69bed5fc0a7a751146554ec239
Jose Vinueza <jose.vinueza@gizlocorp.com>
2015-04-10 11:58:46 -0500

    actualizacion sample

--- dojodwr/src/com/fisa/render/limit/core/LimitView.java
+++ dojodwr/src/com/fisa/render/limit/core/LimitView.java
@@ -1,5 +1,6 @@
 package com.fisa.render.limit.core;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 public class LimitView {
@@ -7,6 +8,13 @@ public class LimitView {
 	private String namePackage;
 	private String aplicaA;
 	List<CustomLevel> custonLevel;
+	private String codeLimit;
+	private String nameLimit;
+	private String moneda;
+	private BigDecimal cupoMaximo;
+	private BigDecimal cupoDefecto;
+	
+	
 	
 	public String getCodePackage() {
 		return codePackage;
@@ -31,6 +39,36 @@ public class LimitView {
 	}
 	public void setAplicaA(String aplicaA) {
 		this.aplicaA = aplicaA;
+	}
+	public String getCodeLimit() {
+		return codeLimit;
+	}
+	public void setCodeLimit(String codeLimit) {
+		this.codeLimit = codeLimit;
+	}
+	public String getNameLimit() {
+		return nameLimit;
+	}
+	public void setNameLimit(String nameLimit) {
+		this.nameLimit = nameLimit;
+	}
+	public String getMoneda() {
+		return moneda;
+	}
+	public void setMoneda(String moneda) {
+		this.moneda = moneda;
+	}
+	public BigDecimal getCupoMaximo() {
+		return cupoMaximo;
+	}
+	public void setCupoMaximo(BigDecimal cupoMaximo) {
+		this.cupoMaximo = cupoMaximo;
+	}
+	public BigDecimal getCupoDefecto() {
+		return cupoDefecto;
+	}
+	public void setCupoDefecto(BigDecimal cupoDefecto) {
+		this.cupoDefecto = cupoDefecto;
 	}
 	
 }
--- dojodwr/src/com/fisa/render/limit/core/Samples.java
+++ dojodwr/src/com/fisa/render/limit/core/Samples.java
@@ -1,5 +1,6 @@
 package com.fisa.render.limit.core;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.GregorianCalendar;
@@ -7,7 +8,7 @@ import java.util.List;
 
 public class Samples {
 
-	public static LimitView getData(){
+	public static LimitView getData(){		
 		LimitView lv =new LimitView();
 		lv.setCodePackage(""+Samples.randBetween(1, 185));
 		lv.setNamePackage("Paquete de Limites y Transacciones");
@@ -30,6 +31,61 @@ public class Samples {
 		lc.add(cl3);
 		lv.setCustonLevel(lc);
 		return lv;
+	}
+	
+	public static List<LimitView> getData2(){
+		List<LimitView> lista = new ArrayList<LimitView>();
+		
+		LimitView lv =new LimitView();
+		lv.setCodePackage(""+Samples.randBetween(1, 185));
+		lv.setNamePackage("Paquete de Limites y Transacciones");
+		lv.setAplicaA("Empresas");
+		List<CustomLevel> lc = new ArrayList<CustomLevel>();
+		CustomLevel cl = new CustomLevel();
+		String txt1 = Samples.randText(randBetween(20, 333));
+		cl.setCode(txt1);
+		cl.setName(txt1);
+		lc.add(cl);
+		CustomLevel cl2 = new CustomLevel();
+		String txt2 = Samples.randText(randBetween(20, 333));
+		cl2.setCode(txt2);
+		cl2.setName(txt2);
+		lc.add(cl2);
+		CustomLevel cl3 = new CustomLevel();
+		String txt3 = Samples.randText(randBetween(20, 333));
+		cl3.setCode(txt3);
+		cl3.setName(txt3);
+		lc.add(cl3);
+		lv.setCustonLevel(lc);
+		lv.setCodeLimit("01");
+		lv.setNameLimit("Limite Diaro");
+		lv.setCupoMaximo(new BigDecimal("1000"));
+		lv.setCupoDefecto(new BigDecimal("5255"));
+		lista.add(lv);
+		
+		LimitView lv2 =new LimitView();
+		lv2.setCodePackage(""+Samples.randBetween(1, 185));
+		lv2.setNamePackage("Paquete de Limites y Transacciones 2");
+		lv2.setAplicaA("Empresas");
+		
+		lv2.setCodeLimit("01");
+		lv2.setNameLimit("Limite Semanal");
+		lv2.setCupoMaximo(new BigDecimal("1000"));
+		lv2.setCupoDefecto(new BigDecimal("5255"));
+		lista.add(lv2);
+		
+		LimitView lv3 =new LimitView();
+		lv3.setCodePackage(""+Samples.randBetween(1, 185));
+		lv3.setNamePackage("Paquete de Limites y Transacciones 3");
+		lv3.setAplicaA("Empresas");
+		
+		lv3.setCodeLimit("01");
+		lv3.setNameLimit("Limite Mensual");
+		lv3.setCupoMaximo(new BigDecimal("1000"));
+		lv3.setCupoDefecto(new BigDecimal("5255"));
+		lista.add(lv3);
+		return lista;
+		
 	}
 	
 	
